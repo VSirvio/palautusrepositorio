b21cad897f8b039eb3453bd057b820697de13030
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-11-13 19:48:15 +0200

    Siisti viikko2/nhl-reader:ssa tulostettavaa pelaajalistaa

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,18 +5,15 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
     players = []
 
     for player_dict in response:
         player = Player(player_dict)
         players.append(player)
 
-    print("Oliot:")
+    print("Players from FIN\n")
 
-    for player in players:
+    for player in sorted(filter(lambda p: p.nationality == "FIN", players), key=lambda p: p.score, reverse=True):
         print(player)
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -5,6 +5,10 @@ class Player:
         self.team = dict['team']
         self.goals = dict['goals']
         self.assists = dict['assists']
+
+    @property
+    def score(self):
+        return self.goals + self.assists
     
     def __str__(self):
-        return f"{self.name} team {self.team}  goals {self.goals} assists {self.assists}"
+        return f"{self.name:20}{self.team}  {self.goals:2} + {self.assists:2} = {self.score:3}"
