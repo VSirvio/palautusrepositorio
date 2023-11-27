2efd1712d5e0a617035e49319f1ae6e3a3973cd8
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-11-27 18:59:28 +0200

    Lisää viikko4/maksukortti-mock:iin testit maksukortin lataamiselle

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_rahan_lataaminen_kortille_onnistuu_jos_summa_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 3
+
+        self.kassa.lataa(maksukortti_mock, 2)
+
+        maksukortti_mock.lataa.assert_called_with(2)
+
+    def test_rahan_lataaminen_kortille_epaonnistuu_jos_summa_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 5
+
+        self.kassa.lataa(maksukortti_mock, -1)
+
+        maksukortti_mock.lataa.assert_not_called()
