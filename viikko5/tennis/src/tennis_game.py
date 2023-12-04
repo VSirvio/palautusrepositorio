1dc21ba909e64a0a421347dff508c339985c43b5
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-12-04 20:02:46 +0200

    Refaktoroi viikko5/tennis/src/tennis_game.py

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,54 @@
 class TennisGame:
+    __WINNING_LIMIT = 4
+
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.__player1_name = player1_name
+        self.__player2_name = player2_name
+        self.__score1 = self.__score2 = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.__player1_name:
+            self.__score1 += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.__score2 += 1
+
+    @classmethod
+    def __get_score_str_when_both_scores_same(cls, score_num):
+        match score_num:
+            case 0: return "Love-All"
+            case 1: return "Fifteen-All"
+            case 2: return "Thirty-All"
+            case _: return "Deuce"
+
+    @classmethod
+    def __get_score_str_when_past_winning_limit(cls, score1, score2):
+        if score1 > score2:
+            score_difference = score1 - score2
+            if score_difference >= 2:
+                return "Win for player1"
+            else:
+                return "Advantage player1"
+        else:
+            score_difference = score2 - score1
+            if score_difference >= 2:
+                return "Win for player2"
+            else:
+                return "Advantage player2"
+
+    @classmethod
+    def __get_score_str_in_basic_case(cls, score_num):
+        match score_num:
+            case 0: return "Love"
+            case 1: return "Fifteen"
+            case 2: return "Thirty"
+            case 3: return "Forty"
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        if self.__score1 == self.__score2:
+            return TennisGame.__get_score_str_when_both_scores_same(self.__score1)
+        elif self.__score1 >= TennisGame.__WINNING_LIMIT or\
+             self.__score2 >= TennisGame.__WINNING_LIMIT:
+            return TennisGame.__get_score_str_when_past_winning_limit(self.__score1, self.__score2)
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+            return TennisGame.__get_score_str_in_basic_case(self.__score1) + "-" +\
+                   TennisGame.__get_score_str_in_basic_case(self.__score2)
