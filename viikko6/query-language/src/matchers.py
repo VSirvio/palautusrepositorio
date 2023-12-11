a3cbf9d5f8da676d13f14af3c11be92a9424ba3b
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-12-11 23:23:02 +0200

    Lisää viikko6/query-language:en operaatio Or

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,16 @@ class And:
 
         return True
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
 
 class PlaysIn:
     def __init__(self, team):
@@ -16,7 +26,6 @@ class PlaysIn:
 
     def test(self, player):
         return player.team == self._team
-
 
 class HasAtLeast:
     def __init__(self, value, attr):
